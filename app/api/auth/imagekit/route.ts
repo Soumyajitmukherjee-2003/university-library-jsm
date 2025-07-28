8918c3e8af3273ae869116427200465a093740b1
Soumyajit-1806 <soumyajitmukherjee2003@gmail.com>
2025-07-28 18:28:32 +0530

    Fix: correctly install @upstash/ratelimit

--- app/api/auth/imagekit/route.ts
+++ app/api/auth/imagekit/route.ts
@@ -3,16 +3,13 @@ import config from "@/lib/config";
 import { NextResponse } from "next/server";
 
 const {
-  imagekit: { publicKey, privateKey, urlEndpoint },
+  env: {
+    imagekit: { publicKey, privateKey, urlEndpoint },
+  },
 } = config;
 
-const imagekit = new ImageKit({
-  publicKey,
-  privateKey,
-  urlEndpoint,
-});
+const imagekit = new ImageKit({ publicKey, privateKey, urlEndpoint });
 
 export async function GET() {
-  const authParams = imagekit.getAuthenticationParameters();
-  return NextResponse.json(authParams);
+  return NextResponse.json(imagekit.getAuthenticationParameters());
 }
