2354a86c236892f76386196d8ccb66798baa88be
Soumyajit-1806 <soumyajitmukherjee2003@gmail.com>
2025-07-28 12:59:10 +0530

    Fix redirect syntax error

--- lib/actions/auth.ts
+++ lib/actions/auth.ts
@@ -1,79 +1,33 @@
-'use server';
+"use server";
 
-import { eq } from "drizzle-orm";
-import { db } from "@/database/drizzle";
-import { todo } from "@/database/schema";
-import { hash } from "bcryptjs";
 import { signIn } from "@/auth";
+import { redirect } from "next/navigation";
 import { headers } from "next/headers";
-import ratelimit from "../ratelimit";
-import { redirect } from "next/dist/server/api-utils";
+import ratelimit from "@/lib/rate-limit";
 
-interface AuthCredentials {
-  fullName: string;
+type Props = {
   email: string;
-  universityId: string;
   password: string;
-  universityCard: string;
-}
+};
 
-export const signInWithCredentials = async (
-  params: Pick<AuthCredentials, "email" | "password">
-) => {
+export const authenticate = async (params: Props) => {
   const { email, password } = params;
   const ip = (await headers()).get("x-forwarded-for") || "127.0.0.1";
+
   const { success } = await ratelimit.limit(ip);
-  if (!success) return redirect( url: '/too-fast');
+  if (!success) return redirect('/too-fast');
 
   try {
     const result = await signIn("credentials", {
+      redirect: false,
       email,
       password,
-      redirect: false,
     });
 
-    if (result?.error) {
-      return { success: false, error: result.error };
-    }
-
-    return { success: true };
+    if (result?.error) throw new Error(result.error);
+    return result;
   } catch (error) {
-    console.log(error, "Signin error");
-    return { success: false, error: "Signin error" };
-  }
-};
-
-export const signUp = async (params: AuthCredentials) => {
-  const { fullName, email, universityId, password, universityCard } = params;
-  const ip = (await headers()).get("x-forwarded-for") || "127.0.0.1";
-  const { success } = await ratelimit.limit(ip);
-  if (!success) return redirect( url: '/too-fast');
-  const existingUser = await db
-    .select()
-    .from(todo)
-    .where(eq(todo.email, email))
-    .limit(1);
-
-  if (existingUser.length > 0) {
-    return { success: false, error: "User already exists" };
-  }
-
-  const hashedPassword = await hash(password, 10);
-
-  try {
-    await db.insert(todo).values({
-      fullName,
-      email,
-      universityId,
-      password: hashedPassword,
-      universityCard,
-    });
-
-    await signInWithCredentials({ email, password });
-
-    return { success: true };
-  } catch (error) {
-    console.log(error, "Signup error");
-    return { success: false, error: "Signup error" };
+    console.error("Authentication error:", error);
+    throw error;
   }
 };
