d741d5c6d7d601ec0d14df0b86d2fb6319b768f2
Soumyajit-1806 <soumyajitmukherjee2003@gmail.com>
2025-07-28 13:18:00 +0530

    Add rate-limit module

--- lib/rate-limit.ts
+++ lib/rate-limit.ts
@@ -1,10 +1,9 @@
-// lib/rate-limit.ts
 import { Ratelimit } from "@upstash/ratelimit";
 import redis from "@/database/redis";
 
 const ratelimit = new Ratelimit({
   redis,
-  limiter: Ratelimit.fixedWindow(5, "1 m"),
+  limiter: Ratelimit.fixedWindow(5, "1m"),
   analytics: true,
   prefix: "@upstash/ratelimit",
 });
